796130d3e573e0655cc0ce365e38fd24bfe84926
Gabriel Sartori <gabriel.martins104@senaisp.edu.br>
2026-08-16 10:20:08 -0300

    Criação do front-end do projeto

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
+
